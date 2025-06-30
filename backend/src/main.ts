5e3a919a3e36249f4878954b56873409dd37ffde
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-06-30 19:40:31 +0300

    configure basic swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Courses')
+    .setDescription('API for courses web app')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger/api', app, documentFactory);
+
   app.use(helmet());
   await app.listen(process.env.PORT ?? 3000);
 }
