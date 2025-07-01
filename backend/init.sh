43018e6c5f9992e0828fe28724f0de930b8b42ff
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-07-01 13:23:19 +0300

    move environments into their own files

--- backend/init.sh
+++ backend/init.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e 
 
+echo "Setting up .env..."
+if [ ! -f ./.env ]; then
+  echo "Creating .env file from sample.env..."
+  cp ./sample.env ./.env
+else
+  echo ".env file already exists, skipping."
+fi
+
 echo "Building NestJS project (generating Swagger schemas...)"
 npm run build
 
