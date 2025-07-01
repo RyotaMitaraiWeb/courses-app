87c8f58eb30522b711e267197e9b1c45273b5450
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-07-01 17:48:18 +0300

    seed some users

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from '../generated/prisma';
+import { User } from '../generated/prisma';
+import * as bcrypt from 'bcrypt';
+
+const PASSWORD_HASH_SALT_ROUNDS = Number(process.env.PASSWORD_HASH_SALT_ROUNDS);
+
+// passwords are unhashed, those are hashed in main()
+const users: User[] = [
+  {
+    id: 1,
+    email: 'john@gmail.com',
+    password: '123456789',
+  },
+  {
+    id: 2,
+    email: 'jane@gmail.com',
+    password: 'nameofmycat',
+  },
+  {
+    id: 3,
+    email: 'linus@outlook.com',
+    password: 'root',
+  },
+];
+
+const prisma = new PrismaClient();
+async function main() {
+  const hash = await bcrypt.genSalt(PASSWORD_HASH_SALT_ROUNDS);
+
+  for (const user of users) {
+    const hashedPassword = await bcrypt.hash(user.password, hash);
+    await prisma.user.upsert({
+      where: { id: user.id },
+      update: {},
+      create: { ...user, password: hashedPassword },
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
