42cc91ec9bfaad0b98337a546540adad9bcc03ef
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-07-02 17:21:05 +0300

    add TruncatePipe

--- frontend/src/app/features/courses/components/course-card/course-card.component.ts
+++ frontend/src/app/features/courses/components/course-card/course-card.component.ts
@@ -1,14 +1,16 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Course } from '../../types';
 import { CourseCardImageComponent } from '../course-card-image/course-card-image.component';
+import { TruncatePipe } from '../../pipes/truncate-pipe';
 
 @Component({
   selector: 'app-course-card',
-  imports: [CourseCardImageComponent],
+  imports: [CourseCardImageComponent, TruncatePipe],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseCardComponent {
   course = input.required<Course>();
+  protected readonly maxDescriptionLength = 120;
 }
