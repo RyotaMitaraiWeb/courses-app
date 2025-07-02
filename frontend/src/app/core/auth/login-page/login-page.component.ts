c20e41f33d9515e51327d6da15562238970f4010
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-07-02 14:22:33 +0300

    add password field

--- frontend/src/app/core/auth/login-page/login-page.component.ts
+++ frontend/src/app/core/auth/login-page/login-page.component.ts
@@ -1,16 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { PageTitleComponent } from '../../../shared/typography/page-title/page-title.component';
 import { FormFieldComponent } from '../../../shared/forms/app-form-field/form-field.component';
 import { Input } from '../../../shared/forms/input';
 import { DraftsIconComponent } from '../../../shared/icons/drafts-icon/drafts-icon.component';
+import { VisibilityIconComponent } from '../../../shared/icons/visibility-icon/visibility-icon.component';
+import { VisibilityOffIconComponent } from '../../../shared/icons/visibility-off-icon/visibility-off-icon.component';
 
 @Component({
   selector: 'app-login-page',
-  imports: [PageTitleComponent, FormFieldComponent, Input, DraftsIconComponent],
+  imports: [PageTitleComponent, FormFieldComponent, Input, DraftsIconComponent, VisibilityIconComponent, VisibilityOffIconComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginPageComponent {
+  passwordIsVisible = signal(false);
 
+  togglePasswordVisibility(event: Event) {
+    event.preventDefault();
+    this.passwordIsVisible.update(v => !v);
+  }
 }
