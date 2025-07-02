07ed510be0c24b1a21f16d010a2c6f6f3af61855
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-07-02 16:02:03 +0300

    add logout button

--- frontend/src/app/features/courses/pages/courses-index/courses-index.component.ts
+++ frontend/src/app/features/courses/pages/courses-index/courses-index.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { PageTitleComponent } from '../../../../shared/typography/page-title/page-title.component';
+import { LogoutButtonComponent } from '../../components/logout-button/logout-button.component';
+import { AuthenticatedOnly } from '../../../../core/auth/directives/authenticated-only/authenticated-only';
 
 @Component({
   selector: 'app-courses-index',
-  imports: [PageTitleComponent],
+  imports: [PageTitleComponent, LogoutButtonComponent, AuthenticatedOnly],
   templateUrl: './courses-index.component.html',
   styleUrl: './courses-index.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
