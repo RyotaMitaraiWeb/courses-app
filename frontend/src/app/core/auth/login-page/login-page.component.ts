7bb1f1d89dcd8ee6c786a5de923ecc69b0e78475
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-07-02 12:43:39 +0300

    create PageTitleComponent

--- frontend/src/app/core/auth/login-page/login-page.component.ts
+++ frontend/src/app/core/auth/login-page/login-page.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PageTitleComponent } from '../../../shared/typography/page-title/page-title.component';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [PageTitleComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- frontend/src/app/features/courses/pages/courses-index/courses-index.component.ts
+++ frontend/src/app/features/courses/pages/courses-index/courses-index.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PageTitleComponent } from "../../../../shared/typography/page-title/page-title.component";
 
 @Component({
   selector: 'app-courses-index',
-  imports: [],
+  imports: [PageTitleComponent],
   templateUrl: './courses-index.component.html',
   styleUrl: './courses-index.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
