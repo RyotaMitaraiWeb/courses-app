e3409da95a0a8169e38aced5eb69f202aeb07606
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-07-02 17:27:49 +0300

    add a non-functional edit button

--- frontend/src/app/features/courses/components/course-card/course-card.component.ts
+++ frontend/src/app/features/courses/components/course-card/course-card.component.ts
@@ -2,10 +2,12 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Course } from '../../types';
 import { CourseCardImageComponent } from '../course-card-image/course-card-image.component';
 import { TruncatePipe } from '../../pipes/truncate-pipe';
+import { EditIconComponent } from '../../../../shared/icons/edit-icon/edit-icon.component';
+import { AuthenticatedOnly } from '../../../../core/auth/directives/authenticated-only/authenticated-only';
 
 @Component({
   selector: 'app-course-card',
-  imports: [CourseCardImageComponent, TruncatePipe],
+  imports: [CourseCardImageComponent, TruncatePipe, EditIconComponent, AuthenticatedOnly],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
